4d9e5c22242eb69c35df2a7eafec6b9b85d4be43
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-01-06 00:09:17 +0100

    added login page

---
+++ pages/login.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect, useRef } from 'react'
+import Grid from '@mui/material/Grid';
+import { Typography, TextField, Button } from '@mui/material';
+import { Box } from '@mui/system';
+import firebaseClient from '../firebaseClient';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+
+
+const Signup = () => {
+    // authRef.current contains auth object from firebase
+    let authRef = useRef(undefined)
+
+    // It is use here to prevent rerender after each user input
+    useEffect(() => {
+        firebaseClient()
+        authRef.current = getAuth()
+    }, [])
+
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [authErrorMessage, setAuthErrormessage] = useState("")
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+
+    // func get trigged when user submit form
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setEmailError(false)
+        setPasswordError(false)
+
+        // check if input is empty. If it is, Textfield style will turn red
+        if (email === "") {
+            setEmailError(true)
+        }
+
+        if (password === "") {
+            setPasswordError(true)
+        }
+
+        // pass email and password to firebase
+        if (email && password) {
+            console.log(email, password)
+            signInWithEmailAndPassword(authRef.current, email, password)
+                .then((cred) => {
+                    console.log("user created:", cred.user)
+                    window.location.href = "/"
+                })
+                .catch(function (error) {
+                    const message = error.message
+                    setAuthErrormessage(message)
+                })
+        }
+
+    }
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "100vh" }}
+        >
+            <Grid item xs={3}>
+                <Typography
+                    variant="h4"
+                    component="h1"
+                    align="center"
+                >
+                    Project Smile
+                </Typography>
+            </Grid>
+            <Grid item xs={3}>
+                <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                    <TextField
+                        onChange={(e) => setEmail(e.target.value)}
+                        label="Email"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                        error={emailError}
+                    />
+                    <TextField
+                        onChange={(e) => setPassword(e.target.value)}
+                        label="Password"
+                        type="password"
+                        variant="outlined"
+                        margin="normal"
+                        fullWidth
+                        required
+                        error={passwordError}
+                    />
+                    <Box textAlign="center" mt={3}>
+                        <Typography>
+                            {authErrorMessage}
+                        </Typography>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                        >
+                            Log in
+                        </Button>
+                    </Box>
+
+                </form>
+            </Grid>
+            <Grid>
+
+            </Grid>
+
+        </Grid>
+    )
+}
+
+export default Signup
