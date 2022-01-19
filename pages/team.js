4e4fca0847740e3a56403382aa4fd542ebb3f100
Patrick Niesen <pknn@wdf.quadox.corp>
2022-01-19 10:46:54 +0100

    mit verbesserung

--- pages/team.js
+++ pages/team.js
@@ -1,64 +1,70 @@
-import * as React from 'react';
 import Stack from '@mui/material/Stack';
+import React from 'react'
+import Layout from '../components/layout'
+import { Typography } from '@mui/material'
+import Head from 'next/head'
 
 
 
-
-function Imagess(){
-    return(
-        <div style={{paddingLeft: 325, paddingTop: 0}}>
+const team = () => {
+    return (
+        <div>
+            <Head>
+                <title>Team</title>
+            </Head>
+            <Layout>
             <div >
                 <h1 style={{hight:250, width:250, paddingTop:10, paddingLeft:475 ,  textAlignVertical: "center"}}> 
                     Unser Team
                     </h1>
                     <Stack direction={{ xs: 'column', sm: 'row' }}
                     spacing={{ xs: 1, sm: 2, md: 4 }}>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:50}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:50}}>
                             <img src="./images/sofie.jpg" alt="" 
-                            Sofie Pischl
-                            style={{hight:150, width:300, paddingTop:20}}/>
-                            <h2>Sofie Pischl</h2>
-                            <p>Developer</p>
+                            style={{ width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Sofie Pischl</Typography>
+                            <Typography>Developer</Typography>
                         </item>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:100}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:100}}>
                             <img src="./images/duc.jpg" alt="" 
-                            style={{ hight:150, width:300, paddingTop:20}}/>
-                            <h2>Viet Duc Kieu</h2>
-                            <p>Developer</p>
+                            style={{ width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Viet Duc Kieu</Typography>
+                            <Typography>Developer</Typography>
                         </item>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:100}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:100}}>
                             <img src="./images/HannahL.jpg" alt="" name="Hannah" 
-                            style={{hight:150, width:300, paddingTop:20}}/>
-                            <h2>Hannah Leier</h2>
-                            <p>Developer</p>
+                            style={{ width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Hannah Leier</Typography>
+                            <Typography>Developer</Typography>
                         </item>
                     </Stack>
                     <Stack direction={{ xs: 'column', sm: 'row' }}
                     spacing={{ xs: 1, sm: 2, md: 4 }}>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:50}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:50}}>
                             <img src="./images/sofie.jpg" alt="" 
-                            style={{hight:150, width:300, paddingTop:20}}/>
-                            <h2>Alisa Rogner</h2>
-                            <p>Scrum Master</p>
+                            style={{ width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Alisa Rogner</Typography>
+                            <Typography>Scrum Master</Typography>
                         </item>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:100}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:100}}>
                             <img src="./images/Patrick.jpg" alt="" 
-                            style={{ hight:150, width:300, paddingTop:20}}/>
-                            <h2>Patrick Niesen</h2>
-                            <p>Projekt Owner</p>
+                            style={{  width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Patrick Niesen</Typography>
+                            <Typography>Projekt Owner</Typography>
                         </item>
-                        <item style={{hight:150, width:300, paddingTop:20, paddingLeft:100}}>
+                        <item style={{ width:300, paddingTop:20, paddingLeft:100}}>
                             <img src="./images/HannahE.jpg" alt="" 
-                            style={{hight:150, width:300, paddingTop:20}}/>
-                            <h2>Hannah Schult</h2>
-                            <p>Developer</p>
+                            style={{ width:300, paddingTop:20}}/>
+                            <Typography variant="h5">Hannah Schult</Typography>
+                            <Typography>Developer</Typography>
                         </item>
                     </Stack>
             </div>
-
+            </Layout>
         </div>
     )
 }
 
+export default team
 
-export default Imagess;
+        
