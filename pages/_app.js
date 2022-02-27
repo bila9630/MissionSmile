25102697190359c93aea974170a04088a9d22e81
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-02-27 21:55:09 +0100

    remove imports from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,15 @@
 import "../styles/globals.css";
 import AuthContextProvider, { AuthContext } from "../contexts/AuthContext";
-import WindowWidth from "../contexts/Bgcontext";
-import { useContext, useState } from "react";
-import { useRouter } from "next/router";
 import MyThemes from "../styles/mytheme";
-import CardMedia from "@mui/material/CardMedia";
 import { ThemeProvider } from "@mui/material/styles";
-import BgWhiteDesktop from "../public/background-white.jpg";
-import BgWhiteMobile from "../public/background-white-mobile.jpg";
-import { Paper } from "@mui/material";
 
 function MyApp({ Component, pageProps }) {
-  const imageUrl = WindowWidth >= 650 ? BgWhiteDesktop : BgWhiteMobile;
-  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
   return (
-    <main
-      sx={{
-        backgroundImage: `url(${imageUrl}`,
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-      }}
-    >
-      <ThemeProvider theme={MyThemes}>
-        <AuthContextProvider>
-          <Component {...pageProps} />
-        </AuthContextProvider>
-      </ThemeProvider>
-    </main>
+    <ThemeProvider theme={MyThemes}>
+      <AuthContextProvider>
+        <Component {...pageProps} />
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
 
