b49565e8993f2d5c5002626a643b8ce8b7fbf87e
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-02-28 10:06:26 +0100

    fix responsive problem on team page

--- pages/team.js
+++ pages/team.js
@@ -1,123 +1,86 @@
 import Stack from "@mui/material/Stack";
 import React from "react";
 import Layout from "../components/layout";
-import { Typography } from "@mui/material";
+import { Avatar, Typography } from "@mui/material";
 import Head from "next/head";
 import Grid from "@mui/material/Grid";
 import Image from "next/image";
 import WindowWidth from "../contexts/Bgcontext";
 
 const Team = () => {
-  //set background images
-  const imageUrl =
-    WindowWidth >= 650
-      ? "./background-white-mobile.jpg"
-      : "./background-white.jpg";
+    //set background images
+    const imageUrl =
+        WindowWidth >= 650
+            ? "./background-white-mobile.jpg"
+            : "./background-white.jpg";
 
-  return (
-    <div>
-      <Head>
-        <title>Team</title>
-      </Head>
-      <Layout>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justify="center"
-          style={{ minHeight: "100vh" }}
-          sx={{
-            backgroundImage: `url(${imageUrl})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        >
-          <h1
-            style={{
-              width: "10%",
-              paddingTop: 10,
-              textAlignVertical: "center",
-            }}
-          >
-            Unser Team
-          </h1>
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={{ xs: 1, sm: 2, md: 4 }}
-          >
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 50 }}>
-              <Image
-                src="/images/sofie.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-              />
-              <Typography variant="h5">Sofie Pischl</Typography>
-              <Typography>Developer</Typography>
-            </item>
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 100 }}>
-              <Image
-                src="/images/duc.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-              />
-              <Typography variant="h5">Viet Duc Kieu</Typography>
-              <Typography>Developer</Typography>
-            </item>
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 100 }}>
-              <Image
-                src="/images/HannahL.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-              />
-              <Typography variant="h5">Hannah Leier</Typography>
-              <Typography>Developer</Typography>
-            </item>
-          </Stack>
-          <Stack
-            direction={{ xs: "column", sm: "row" }}
-            spacing={{ xs: 1, sm: 2, md: 4 }}
-          >
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 50 }}>
-              <Image
-                src="/images/Alisa.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-                flex="1"
-                resizeMode="contain"
-              />
-              <Typography variant="h5">Alisa Rogner</Typography>
-              <Typography>Scrum Master</Typography>
-            </item>
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 100 }}>
-              <Image
-                src="/images/Patrick.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-              />
-              <Typography variant="h5">Patrick Niesen</Typography>
-              <Typography>Projekt Owner</Typography>
-            </item>
-            <item style={{ width: 300, paddingTop: 20, paddingLeft: 100 }}>
-              <Image
-                src="/images/HannahE.jpg"
-                alt="logo"
-                width="300"
-                height="300"
-              />
-              <Typography variant="h5">Hannah Schult</Typography>
-              <Typography>Developer</Typography>
-            </item>
-          </Stack>
-        </Grid>
-      </Layout>
-    </div>
-  );
+    return (
+        <div>
+            <Head>
+                <title>Team</title>
+            </Head>
+            <Layout>
+
+                <Typography variant="h4" textAlign="center" m={2}>Our team</Typography>
+                <Grid
+                    container
+                    spacing={0}
+                    direction="row"
+                    justifyContent="center"
+                    textAlign="center"
+                    style={{ minHeight: "100vh" }}
+                    sx={{
+                        backgroundImage: `url(${imageUrl})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: "cover",
+                    }}
+                >
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar mx="auto" style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/sofie.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Sofie Pischl</Typography>
+                        <Typography>Developer</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/duc.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Viet Duc Kieu</Typography>
+                        <Typography>Developer</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/HannahL.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Hannah Laier</Typography>
+                        <Typography>Developer</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/Alisa.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Alisa Rogner</Typography>
+                        <Typography>Scrum Master</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/Patrick.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Patrick Niesen</Typography>
+                        <Typography>Projekt Owner</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={4} align="center">
+                        <Avatar style={{ width: 280, height: 280, border: "0.8vh solid orange" }}>
+                            <Image src="/images/HannahE.jpg" alt="logo" width="280" height="280" />
+                        </Avatar>
+                        <Typography variant="h5">Hannah Schult</Typography>
+                        <Typography>Developer</Typography>
+                    </Grid>
+                </Grid>
+            </Layout>
+        </div >
+    );
 };
 
 export default Team;
