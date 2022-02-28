7aedf35505b2f4ae92d0f3ca35f587d36a5e248b
sofietheresa <sofl3905@gmail.com>
2022-02-28 11:56:55 +0100

    updated layout pricing page & background home page

--- components/pricingCard.js
+++ components/pricingCard.js
@@ -1,22 +1,61 @@
-import React from 'react'
-import { Grid, Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 const PricingCard = ({ number, time, price }) => {
-    return (
-        <Grid item xs={12} sm={6} md={4}
-            sx={{ backgroundColor: "primary.background", border: 4, borderRadius: 10, borderColor: "primary.backgroundDark" }}
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        item
+        container
+        spacing={2}
+        columns={{ xs: "12rem", sm: "6rem", md: "4 rem" }}
+        sx={{
+          backgroundColor: "primary.light",
+          border: "0.8vh solid orange",
+          borderRadius: 10,
+          borderColor: "primary.backgroundDark",
+          marginLeft: "2rem",
+          marginRight: "2rem",
+          marginBottom: "2rem",
+          paddingLeft: "2rem",
+          paddingRight: "2rem",
+          maxWidth: "40rem",
+          minHeight: "50rem",
+        }}
+      >
+        <Typography
+          variant="h4"
+          mt={"3rem"}
+          sx={{ textAlign: "center", width: "100rem" }}
         >
-            <Typography variant="h4" textAlign="center" my={"2rem"}>Paket {number} </Typography>
-            <Typography mb={"1rem"}>Sicherung und Analyse der Daten sowie ein abschließendes Beratungsgespräch</Typography>
-            <Typography mb={"1rem"}>Anfragen werden innerhalb von maximal 2 Wochen bearbeitet</Typography>
-            <Typography mb={"1rem"}>Technischer Service durch: First Level Support, Second Level Support und Third Level Support</Typography>
-            <Typography mb={"1rem"}>Die App ist 24/7 verfügbar, der Service Wochentags 08:00 – 21:00 (MEZ) Samstags 08:00 – 14:00 (MEZ)</Typography>
-            <Typography mb={"2rem"}>Kündigungsfrist: 3 Monate</Typography>
-            <Typography variant="h5" mb={"2rem"} >Laufzeit: {time} Monate</Typography>
-            <Typography variant="h4" textAlign="center">Preis: {price}€ pro Monat</Typography>
-        </Grid>
-    )
-}
+          <b>Paket {number} </b>
+        </Typography>
+        <Typography mb={"1rem"}>
+          Sicherung und Analyse der Daten sowie ein abschließendes
+          Beratungsgespräch
+        </Typography>
+        <Typography mb={"1rem"}>
+          Anfragen werden innerhalb von maximal 2 Wochen bearbeitet
+        </Typography>
+        <Typography mb={"1rem"}>
+          Technischer Service durch: First Level Support, Second Level Support
+          und Third Level Support
+        </Typography>
+        <Typography mb={"1rem"}>
+          Die App ist 24/7 verfügbar, der Service Wochentags 08:00 – 21:00 (MEZ)
+          Samstags 08:00 – 14:00 (MEZ)
+        </Typography>
+        <Typography mb={"1rem"}>Kündigungsfrist: 3 Monate</Typography>
+        <Typography variant="h5" mb={"2rem"} mt={"5rem"}>
+          Laufzeit: {time} Monate
+        </Typography>
+        <Typography variant="h4" mb={"2rem"} textAlign="center">
+          Preis: {price}€ pro Monat
+        </Typography>
+      </Grid>
+    </Box>
+  );
+};
 
-export default PricingCard
+export default PricingCard;
